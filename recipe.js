c2e1539337a9ac4eca43c90e083e578636d37a62
Flatiron School <github email address>
2016-10-21 17:32:05 -0400

    done

--- recipe.js
+++ recipe.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function addIngredient(recipe, ingredient_name, amount) {
+  recipe[`${ingredient_name}`] = amount
+  return recipe
+}
+
+
+function removeIngredient(recipe, ingredient) {
+  delete recipe[`${ingredient}`]
+  return recipe
+}
+
+
+function updateIngredient(recipe, ingredient, amount) {
+  recipe[`${ingredient}`] = amount
+  return recipe
+}
+
+
+function readRecipe(recipe) {
+  for ( var a in recipe) {
+    console.log(`this recipe calls for ${recipe[a]} of ${a}`)
+  }
+}
